69e552bfcf9d03b183f31cc88923804d8c1cf9d8
Andrey Sadkov <ad.sadkov@gmail.com>
2024-10-30 11:29:58 +0300

    refactored: corrected tabs

--- lua/andrey/plugins/lsp/mason.lua
+++ lua/andrey/plugins/lsp/mason.lua
@@ -4,27 +4,27 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
-    config = function()
-        local mason_ok, mason = pcall(require, "mason")
-        if not mason_ok then
-            vim.notify("Failed to load mason.", vim.log.levels.ERROR)
-            return
-        end
+	config = function()
+		local mason_ok, mason = pcall(require, "mason")
+		if not mason_ok then
+			vim.notify("Failed to load mason.", vim.log.levels.ERROR)
+			return
+		end
 
-        local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
-        if not mason_lspconfig_ok then
-            vim.notify("Failed to load mason-lspconfig.", vim.log.levels.ERROR)
-            return
-        end
+		local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+		if not mason_lspconfig_ok then
+			vim.notify("Failed to load mason-lspconfig.", vim.log.levels.ERROR)
+			return
+		end
 
-        local mason_tool_installer_ok, mason_tool_installer = pcall(require, "mason-tool-installer")
-        if not mason_tool_installer_ok then
-            vim.notify("Failed to load mason-tool-installer.", vim.log.levels.ERROR)
-            return
-        end
+		local mason_tool_installer_ok, mason_tool_installer = pcall(require, "mason-tool-installer")
+		if not mason_tool_installer_ok then
+			vim.notify("Failed to load mason-tool-installer.", vim.log.levels.ERROR)
+			return
+		end
 
-        -- configure mason ui
-        mason.setup({
+		-- configure mason ui
+		mason.setup({
 			ui = {
 				icons = {
 					package_installed = "✓",
@@ -34,30 +34,30 @@ return {
 			},
 		})
 
-        -- mason-lspconfig installs the following language servers
-        mason_lspconfig.setup({
-            ensure_installed = {
-                "pyright",  -- python language server
-                "gopls",    -- golang language server
-                "ts_ls",    -- js & ts language server
-            },
-        })
+		-- mason-lspconfig installs the following language servers
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"pyright", -- python language server
+				"gopls", -- golang language server
+				"ts_ls", -- js & ts language server
+			},
+		})
 
-        -- mason-tool-installer installs the following external tools
-        mason_tool_installer.setup({
-            ensure_installed = {
-                "goimports",        -- go import formatter
-                "golines",          -- go formatter for long lines
-                "golangci-lint",    -- go linter
+		-- mason-tool-installer installs the following external tools
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"goimports", -- go import formatter
+				"golines", -- go formatter for long lines
+				"golangci-lint", -- go linter
 
-                "black",            -- python formatter
-                "pylint",           -- python linter
+				"black", -- python formatter
+				"pylint", -- python linter
 
-                "prettier",         -- formatter for js and other languages
-                "eslint_d",         -- js linter
+				"prettier", -- formatter for js and other languages
+				"eslint_d", -- js linter
 
-                "stylua",           -- lua formatter
-            },
-        })
-    end,
+				"stylua", -- lua formatter
+			},
+		})
+	end,
 }
